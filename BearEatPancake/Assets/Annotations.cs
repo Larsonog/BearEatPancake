da67e4c24660006f8a832584977e84879cd4f597
olivia <larsonog@hendrix.edu>
2021-02-01 23:47:43 -0600

    Added in bear and cloud images

---
+++ BearEatPancake/Assets/Annotations.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Annotations : MonoBehaviour
+{
+    // references 
+    // bear https://opengameart.org/content/bear-0
+    // cloud https://opengameart.org/content/cloud
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
