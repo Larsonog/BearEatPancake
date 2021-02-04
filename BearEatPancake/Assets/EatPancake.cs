23be5e2f46749122503e59372028d23ca2de6ba7
olivia <larsonog@hendrix.edu>
2021-02-03 21:03:51 -0600

    Collisions and tears

---
+++ BearEatPancake/Assets/EatPancake.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatPancake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start time!");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("munch");
+        Destroy(collision.gameObject);
+    }
+}
