f7250fe3e58f252e5d974b58ec85f5d47fe7ea92
olivia <larsonog@hendrix.edu>
2021-02-03 19:19:03 -0600

    Changed pancake drop function

--- BearEatPancake/Assets/droppancakes.cs
+++ BearEatPancake/Assets/droppancakes.cs
@@ -11,22 +11,59 @@ public class droppancakes : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("droppancake1", 2f, .56f);
-        InvokeRepeating("droppancake2", 2.5f, .5f);
-        InvokeRepeating("droppancake3", 3f, .75f);
+        StartCoroutine("DropPancakeOne");
+        StartCoroutine("DropPancakeTwo");
+        StartCoroutine("DropPancakeThree");
+        //InvokeRepeating("droppancake1", 2f, .56f);
+        //InvokeRepeating("droppancake2", 2.5f, .5f);
+        //InvokeRepeating("droppancake3", 3f, .75f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    void droppancake1() { 
-        Instantiate(pancake1); }
+    IEnumerator DropPancakeOne()
+    {
+        yield return new WaitForSeconds(2f);
+        for (; ; )
+        {
+            Instantiate(pancake1);
+            yield return new WaitForSeconds(Random.Range(2f, 4f));
+        }
+    }
+    IEnumerator DropPancakeTwo()
+    {
+        yield return new WaitForSeconds(2f);
+        for (; ; )
+        {
+            Instantiate(pancake2);
+            yield return new WaitForSeconds(Random.Range(2f, 4f));
+        }
+    }
+    IEnumerator DropPancakeThree()
+    {
+        yield return new WaitForSeconds(2f);
+        for (; ; )
+        {
+            Instantiate(pancake3);
+            yield return new WaitForSeconds(Random.Range(2f, 4f));
+        }
+    }
 
-    void droppancake2() { 
-        Instantiate(pancake2); }
+    void droppancake1()
+    {
+        Instantiate(pancake1);
+    }
 
-    void droppancake3() { 
-        Instantiate(pancake3); }
+    void droppancake2()
+    {
+        Instantiate(pancake2);
+    }
+
+    void droppancake3()
+    {
+        Instantiate(pancake3);
+    }
 }
