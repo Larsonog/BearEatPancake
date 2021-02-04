902230e0f55c8f6de9e37db4190d8dac37bef365
olivia <larsonog@hendrix.edu>
2021-02-03 21:47:55 -0600

    Game Manager now works!!!!!!!!!!!!!

---
+++ BearEatPancake/Assets/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static GameManager Instance { get; private set; }
+    private int score = 0;
+    public TextMeshProUGUI scoreText;
+
+    private void Awake()
+    {
+       if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void IncSCore(int ds)
+    {
+        //score += ds;
+        //scoreText.text = "Score: " + score;
+    }
+    void Start()
+    {
+        scoreText.text = "I'm awake";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
